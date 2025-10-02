1ea36545fc8e402b0c9e685d3455df326043b61b
Michael-Howes <50608711+Michael-Howes@users.noreply.github.com>
2025-10-02 18:39:10 +0000

    add test for additional functions in example module

--- tests/unit/test_example.py
+++ tests/unit/test_example.py
@@ -7,7 +7,7 @@ is up to you. This test example provides a single test for the example.py
 module.
 """
 
-from pyospackage_mhowes.example import add_numbers
+from pyospackage_mhowes.example import add_numbers, subtract_numbers, add_list_of_numbers
 
 def test_add_numbers():
     """
@@ -18,3 +18,35 @@ def test_add_numbers():
     out = add_numbers(1, 2)
     expected_out = 3
     assert  out == expected_out, f"Expected {expected_out} but got {out}"
+
+def test_subtract_numbers():
+    """
+    Test that substract_numbers works as expected.
+    """
+    out = subtract_numbers(3, 5)
+    expected_out = -2
+    assert out == expected_out, f"Expected {expected_out} but got {out}"
+
+def test_add_list_of_numbers():
+    """
+    Test that test_add_list_of_numbers works as expected.
+    """
+    out = add_list_of_numbers([3, 5])
+    expected_out = 8
+    assert out == expected_out, f"Expected {expected_out} but got {out}"
+
+def test_add_list_of_numbers_with_empty_list():
+    """
+    Test that test_add_list_of_numbers works on edge case of empty list.
+    """
+    out = add_list_of_numbers([])
+    expected_out = 0
+    assert out == expected_out, f"Expected {expected_out} but got {out}"
+
+def test_add_list_of_numbers_with_singleton():
+    """
+    Test that test_add_list_of_numbers works on edge case of singleton.
+    """
+    out = add_list_of_numbers([2])
+    expected_out = 2
+    assert out == expected_out, f"Expected {expected_out} but got {out}"
